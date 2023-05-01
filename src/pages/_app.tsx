1f34b241d42336b5fc16555aac82b4c9ed2982c4
UniUrchin <85057870+UniUrchin@users.noreply.github.com>
2023-05-02 01:37:44 +0900

    :zap: Prettierを設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import NextHead from "next/head";
-import { AppProps } from 'next/app';
+import { AppProps } from "next/app";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,9 @@ import { NextPage } from "next";
 const IndexPage: NextPage = () => {
   return (
     <div>
-      <h1 className="text-2xl font-mplus">SMBR Cockpit Sample</h1>
+      <h1 className="font-mplus text-2xl">SMBR Cockpit Sample</h1>
     </div>
-  )
-}
+  );
+};
 
 export default IndexPage;
