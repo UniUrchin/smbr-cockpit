77912a19f2806cd1240ff5937768cb05ef89bf99
UniUrchin <85057870+UniUrchin@users.noreply.github.com>
2023-05-02 16:17:44 +0900

    :sparkles: SMBR Cockpitの雛形完成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,31 @@
 import { NextPage } from "next";
 import { useState, useEffect } from "react";
-import init, { hello } from "@/wasm/pkg/wasm.js";
+import init, { run } from "@/wasm/pkg/wasm.js";
 
 const IndexPage: NextPage = () => {
-  const [result, setResult] = useState("");
+  const [text, setText] = useState<string>("");
+  const [result, setResult] = useState<string>("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(event.target.value);
+  };
 
   useEffect(() => {
     (async () => {
       await init();
-      setResult(hello());
+      setResult(run(text, ""));
     })();
-  }, []);
+  }, [text]);
 
   return (
-    <div className="font-mplus1">
-      <h1 className="text-2xl">SMBR Cockpit Sample</h1>
-      <p>{result}</p>
+    <div className="flex flex-col items-center justify-center font-mplus1">
+      <h1 className="m-8 text-2xl">SMBR Cockpit Sample</h1>
+      <textarea
+        value={text}
+        onChange={handleChange}
+        className="m-8 h-[20rem] w-[60rem] bg-slate-100"
+      />
+      <div className="text-xl">Result: {result}</div>
     </div>
   );
 };
