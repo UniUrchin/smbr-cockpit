a688e13e38619f32ad54054cb30ff332a5ac0c71
UniUrchin <85057870+UniUrchin@users.noreply.github.com>
2023-05-02 14:30:37 +0900

    :construction: WASMの結果をIndexPageに表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,21 @@
 import { NextPage } from "next";
+import { useState, useEffect } from "react";
+import init, { hello } from "@/wasm/pkg/wasm.js";
 
 const IndexPage: NextPage = () => {
+  const [result, setResult] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      await init();
+      setResult(hello());
+    })();
+  }, []);
+
   return (
-    <div>
-      <h1 className="font-mplus text-2xl">SMBR Cockpit Sample</h1>
+    <div className="font-mplus">
+      <h1 className="text-2xl">SMBR Cockpit Sample</h1>
+      <p>{result}</p>
     </div>
   );
 };
